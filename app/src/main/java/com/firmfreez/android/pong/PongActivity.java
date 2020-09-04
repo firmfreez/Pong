01a3c8d07d559c2ec6482f90f042197fab100ce6
FirmFreez <37814484+firmfreez@users.noreply.github.com>
2020-09-04 12:43:14 +0300

    Add mPongGame to PongActivity.java Add SurfaceView to PongGame.java constructor

--- app/src/main/java/com/firmfreez/android/pong/PongActivity.java
+++ app/src/main/java/com/firmfreez/android/pong/PongActivity.java
@@ -2,13 +2,36 @@ package com.firmfreez.android.pong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
 
 public class PongActivity extends AppCompatActivity {
+
+    private PongGame mPongGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Point displaySize = new Point();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            getWindowManager().getDefaultDisplay().getRealSize(displaySize);
+        } else {
+            getWindowManager().getDefaultDisplay().getSize(displaySize);
+        }
+
+        mPongGame = new PongGame(this, displaySize.x, displaySize.y);
+        setContentView(mPongGame);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
--- app/src/main/java/com/firmfreez/android/pong/PongGame.java
+++ app/src/main/java/com/firmfreez/android/pong/PongGame.java
@@ -1,4 +1,19 @@
 package com.firmfreez.android.pong;
 
-public class PongGame {
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class PongGame extends SurfaceView {
+
+    /**
+     * Конструктор (используйте new PongGame(this, size.x, size.y) для вызова
+     * @param context - контекст, необходимый для SurfaceView
+     * @param x - Ширина экрана
+     * @param y - Высота экрана
+     */
+    public PongGame(Context context, int x, int y) {
+        super(context);
+    }
+
+
 }
